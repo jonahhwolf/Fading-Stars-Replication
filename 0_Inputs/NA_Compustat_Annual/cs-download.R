f6bf24ff02d206ad5d74c3d59f6ae9fffaea0ce5
Jonah Wolf <jonahwolf@MacBook-Air-240.local>
2024-12-06 16:52:29 -0500

    Duplicated comments from original package

--- 0_Inputs/NA_Compustat_Annual/cs-download.R
+++ 0_Inputs/NA_Compustat_Annual/cs-download.R
@@ -1,3 +1,5 @@
+# Downloads Compustat, CRSP and linking table from WRDS
+
 library(RPostgres)
 library(data.table)
 library(tidyverse)
@@ -5,7 +7,7 @@ library(this.path)
 
 setwd(dirname(this.path()))
 
-# create first csv
+# Open DB connection
 wrds <- dbConnect(Postgres(),
                   host='wrds-pgdata.wharton.upenn.edu',
                   port=9737,
@@ -13,12 +15,15 @@ wrds <- dbConnect(Postgres(),
                   sslmode='require',
                   user='jonahwolf')
 
+# COMPUSTAT
+  
+# COMPANY INFORMATION
 res <- dbSendQuery(wrds, "SELECT gvkey, loc, dlrsn, ipodate, sic, naics FROM comp_na_daily_all.company")
 company <- dbFetch(res, n = 2000)
 fwrite(company, "./raw/company.csv")
 dbClearResult(res)
 
-# create second csv
+# NA FUNDA
 res <- dbSendQuery(wrds, "SELECT gvkey, consol, indfmt, datafmt, popsrc, curcd, fyear, datadate, conm, at, cogs, csho, dlc, dltt, emp, ib, pstk, sale, xlr, xsga, fic, prcc_f, prcc_c, oiadp, fyr FROM comp_na_daily_all.funda")
 funda <- dbFetch(res, n = 2000)
 dbClearResult(res)
@@ -32,13 +37,15 @@ filtered_funda <- funda |>
 
 fwrite(filtered_funda, "./raw/funda.csv")
 
-# Load CRSP-Compustat Linking Table
+# CRSP-COMPUSTAT
+
+# CRSP-COMPUSTAT LINKING TABLE
 res <- dbSendQuery(wrds, "SELECT * FROM crsp_a_ccm.ccmxpf_linktable")
 linktable_data <- dbFetch(res, n = 2000)
 dbClearResult(res)
 fwrite(linktable_data, "./raw/ccmxpf_linktable.csv")
 
-# Load CRSP Price & Shares
+# CRSP PRICE & SHARES
 res <- dbSendQuery(wrds, "SELECT permno, permco, date, prc, shrout FROM crsp.msf")
 crsp_msf <- dbFetch(res, n = 2000)
 dbClearResult(res)
