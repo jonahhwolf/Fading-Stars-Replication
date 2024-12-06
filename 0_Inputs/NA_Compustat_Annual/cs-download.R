87cdbd70c06f8285b286b1585262e3a425c31b97
Jonah Wolf <jonahwolf@MacBook-Air-240.local>
2024-12-06 16:36:31 -0500

    Updated .gitignore, re-arranged files

--- 0_Inputs/NA_Compustat_Annual/cs-download.R
+++ 0_Inputs/NA_Compustat_Annual/cs-download.R
@@ -11,13 +11,13 @@ wrds <- dbConnect(Postgres(),
                   user='jonahwolf')
 
 res <- dbSendQuery(wrds, "SELECT gvkey, loc, dlrsn, ipodate, sic, naics FROM comp_na_daily_all.company")
-company <- dbFetch(res, n = 10)
-fwrite(company, "./Data/NA_Compustat_Annual/raw/company.csv")
+company <- dbFetch(res, n = 2000)
+fwrite(company, "./raw/company.csv")
 dbClearResult(res)
 
 # create second csv
 res <- dbSendQuery(wrds, "SELECT gvkey, consol, indfmt, datafmt, popsrc, curcd, fyear, datadate, conm, at, cogs, csho, dlc, dltt, emp, ib, pstk, sale, xlr, xsga, fic, prcc_f, prcc_c, oiadp, fyr FROM comp_na_daily_all.funda")
-funda <- dbFetch(res, n = 50)
+funda <- dbFetch(res, n = 2000)
 dbClearResult(res)
 
 filtered_funda <- funda |>
@@ -27,19 +27,19 @@ filtered_funda <- funda |>
            popsrc == "D" & 
            curcd == "USD")
 
-fwrite(filtered_funda, "./Data/NA_Compustat_Annual/raw/funda.csv")
+fwrite(filtered_funda, "./raw/funda.csv")
 
 # Load CRSP-Compustat Linking Table
 res <- dbSendQuery(wrds, "SELECT * FROM crsp_a_ccm.ccmxpf_linktable")
-linktable_data <- dbFetch(res, n = 50)
+linktable_data <- dbFetch(res, n = 2000)
 dbClearResult(res)
-fwrite(linktable_data, "./Data/NA_Compustat_Annual/raw/ccmxpf_linktable.csv")
+fwrite(linktable_data, "./raw/ccmxpf_linktable.csv")
 
 # Load CRSP Price & Shares
 res <- dbSendQuery(wrds, "SELECT permno, permco, date, prc, shrout FROM crsp.msf")
-crsp_msf <- dbFetch(res, n = 50)
+crsp_msf <- dbFetch(res, n = 2000)
 dbClearResult(res)
-fwrite(crsp_msf, "./Data/NA_Compustat_Annual/raw/crsp_msf.csv")
+fwrite(crsp_msf, "./raw/crsp_msf.csv")
 
 # Close database connection
 dbDisconnect(wrds)
