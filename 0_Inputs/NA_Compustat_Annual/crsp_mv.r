d61b73cd60840304937577025baf81e8891e13ac
Jonah Wolf <jonahwolf@MacBook-Air-240.local>
2024-12-08 19:07:30 -0500

    Calculating market values

--- 0_Inputs/NA_Compustat_Annual/crsp_mv.r
+++ 0_Inputs/NA_Compustat_Annual/crsp_mv.r
@@ -22,12 +22,17 @@
 # rename permno lpermno
 # save tempcrsp.dta, replace
 library(data.table)
+library(tidyverse)
+library(this.path)
+
+setwd(dirname(this.path()))
 
 crsp <- read.csv("raw/crsp_msf.csv") |>
   filter(!is.na(prc)) |>
   rename(lpermno = permno)
 
-fwrite(crsp, "./raw/tempcrsp.csv")
+anyDuplicated(crsp)
+
 # * map to compustat
 # use gvkey linkdt linkenddt lpermno if ~missing(lpermno) using "NA_Compustat_Annual/raw/ccmxpf_linktable.dta", clear
 # bys lpermno linkdt (linkenddt): keep if _n == _N
@@ -35,19 +40,45 @@ fwrite(crsp, "./raw/tempcrsp.csv")
 # drop if missing(date)
 # keep gvkey date lpermno prc shrout
 # duplicates drop
-# 
+
+linktable <- read.csv("raw/ccmxpf_linktable.csv") |>
+  filter(!is.na(lpermno)) |>
+  select(gvkey, linkdt, linkenddt, lpermno) |>
+  arrange(lpermno, linkdt, desc(linkenddt)) |>
+  group_by(lpermno) |>
+  slice_tail(n = 1) |>
+  ungroup()
+
+crsp_cpstat <- inner_join(crsp, linktable, by = "lpermno") |>
+  filter(date >= linkdt & date <= linkenddt) |>
+  select(gvkey, date, lpermno, prc, shrout) |>
+  distinct()
+
 # * Generate year and month variables
 # gen year = year(date)
 # gen month = month(date)
 # sort gvkey year month
-# 
+
+crsp_cpstat <- crsp_cpstat |>
+  mutate(year = year(date), month = month(date)) |>
+  arrange(gvkey, year, month)
+
 # * ensure single map per permno
 # preserve
-# 	keep gvkey lpermno year month 
-# 	isid gvkey lpermno year month 
+# 	keep gvkey lpermno year month
+# 	isid gvkey lpermno year month
 # restore
 # save tempcrsp, replace
-# 
+
+tempcrsp <- crsp_cpstat |>
+  select(gvkey, lpermno, year, month) |>
+  distinct() |>
+  group_by(gvkey, lpermno, year, month) |>
+  filter(n() == 1) |>
+  ungroup()
+
+fwrite(tempcrsp, "raw/tempcrsp.csv")
+
 # **
 # 
 # /* -------------------- */ 
@@ -63,10 +94,23 @@ fwrite(crsp, "./raw/tempcrsp.csv")
 # by gvkey year month: keep if _n == _N
 # replace me_crsp = me_crsp/1000
 # lab var me_crsp "Market value of Equity (m$, from CRSP)"
-# 
+
+crsp_cpstat_mv <- crsp_cpstat |>
+  mutate(
+    prc = abs(prc),
+    market_value = prc * shrout
+    ) |>
+  group_by(gvkey, year, month) |>
+  summarize(me_crsp = sum(market_value, na.rm = TRUE)) |>
+  filter(!is.na(me_crsp)) |>
+  slice_tail(n = 1) |>
+  mutate(me_crsp = me_crsp / 1000) |>
+  ungroup()
+
 # * finalize
 # keep gvkey lpermno year month me_crsp 
 # order gvkey lpermno year month me_crsp 
 # save "NA_Compustat_Annual/loaded/crsp_cpstat_mv.dta", replace
 # erase tempcrsp.dta
 # 
+fwrite(crsp_cpstat_mv, "crsp_cpstat_mv.csv")
