3b60e97e65c6e027dbd43a600caf5704cba25d4f
Jonah Wolf <jonahwolf@Mac-14959.lan>
2026-01-22 13:30:43 -0500

    Fixing variable name

--- 2_Codes/build/main_datawork.R
+++ 2_Codes/build/main_datawork.R
@@ -110,7 +110,7 @@ tempfirm <- tempfirm |>
 # replace mneind_sic = "TCU" if inlist(mneind_sic,"Communications","Electric, gas, and sanitary services","Transportation") & year <= 1988
 # replace mneind_sic = "CU" if inlist(mneind_sic,"Communications","Electric, gas, and sanitary services") & inrange(year,1989,1993)
 # save tempfirm, replace
-cstat_bea <- cstat_bea |>
+tempfirm <- tempfirm |>
   mutate(sicbea = ifelse(sic2 %in% c(37, 48), sic3, sic2))
 # 
 # **
