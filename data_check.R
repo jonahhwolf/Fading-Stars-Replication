66634fbe99eb83c67fcc53449f0961654bd6bded
Jonah Wolf <jonahwolf@Mac-18587.lan>
2026-02-07 11:25:16 -0500

    Worked on write-up and data check

--- data_check.R
+++ data_check.R
@@ -5,20 +5,25 @@ library(tidyverse)
 data_check <- function(filepath){
   r_data <- read_csv(paste0(dirname(this.path()), "/", filepath, ".csv"))
   stata_data <- read_dta(paste0(dirname(this.path()), "/", filepath, ".dta")) |>
-    relocate(names(r_data))
+    relocate(names(r_data)) |>
+    mutate(across(everything(), ~ ifelse(.x == "", NA, .x)))
   all.equal(r_data, stata_data, check.attributes = FALSE)
 }
 
 data_check("Temp/naics2bea")
+# TRUE
 
 data_check("Temp/bea2industry")
-# error just a function of how R reads empty values
+# TRUE
+
+data_check("0_Inputs/Fred/loaded/fred_data")
+# lengths differ
 
 data_check("0_Inputs/US_BEA_Main/loaded/BEA_industry_raw")
 # miniscule difference
 
 data_check("Temp/BEA_mapped")
-# seems mostly right
+# miniscule difference
 
 data_check("0_Inputs/NA_Compustat_Annual/raw/crsp_msf")
 # different lengths but otherwise seems OK
@@ -28,6 +33,8 @@ data_check("0_Inputs/NA_Compustat_Annual/raw/ccmxpf_linktable")
 
 data_check("3_Final_Data/main_dataset_firm")
 # seems alright?
+
+data_check("Temp/tempanalysis_stars")
 
 data_check("0_Inputs/NA_Compustat_Annual/loaded/NA_compustat")
 # seems OK?
